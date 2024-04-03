e10883365163bfb51450f8785c5b82db63986e9e
Julius Adebowale Gabriel <juadebgabriel@gmail.com>
2024-04-03 01:11:25 +0100

    fix

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -29,14 +29,19 @@ const Hero = () => {
   useGSAP(() => {
     gsap.to("#hero", {
       opacity: 1,
-      delay: 1.5,
+      delay: 2,
+    });
+    gsap.to("#cta", {
+      opacity: 1,
+      y: -50,
+      delay: 2,
     });
   }, []);
   return (
     <section className="w-full nav-height bg-black relative">
       <div className="h-5/6 w-full flex-center flex-col">
         <p id="hero" className="hero-title">
-          Iphone 15 Pro
+          iPhone 15 Pro
         </p>
         <div className="md:w-10/12 w-9/12">
           <video
@@ -50,6 +55,16 @@ const Hero = () => {
             <source src={videoSrc} type="video/mp4" />
           </video>
         </div>
+      </div>
+      <div
+        id="cta"
+        className="flex flex-col items-center opacity-0 translate-y-20"
+      >
+        <a href="#highlight" className="btn">
+          Buy
+        </a>
+
+        <p className="font-normal text-xl">From $199/month or $999</p>
       </div>
     </section>
   );
